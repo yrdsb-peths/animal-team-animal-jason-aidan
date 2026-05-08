f1b6e6af63de099c71dd5f88e8a58c870f3e9d74
Jason <baifnjdn@gmail.com>
2026-05-08 11:35:29 -0400

    Added subclass to make asteroid game
    removed elephant
    added projectile, ship, and asteroid

--- Projectile.java
+++ Projectile.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Elephant here.
+ * Write a description of class Projectile here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Elephant extends Actor
+public class Projectile extends Actor
 {
     /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * Act - do whatever the Projectile wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
