dcc90c77e49682885a7ce0f507e34a1fc7fa1ea7
Jason <baifnjdn@gmail.com>
2026-05-08 11:47:53 -0400

    Added Ship Movement

--- Projectile.java
+++ Projectile.java
@@ -12,33 +12,9 @@ public class Projectile extends Actor
      * Act - do whatever the Projectile wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    static int SPEED = 10;
-    double direction = 0; // in radians
-
+    
     public void act()
     {
         // Add your action code here.
-
-        if (Greentfoot.isKeyDown("space"))
-        {
-            move();
-        }
-
-        if (Greenfoot.isKeyDown("left"))
-        {
-            direction -= 0.1; // rotate left
-        }
-        if (Greenfoot.isKeyDown("right"))
-        {            
-            direction += 0.1; // rotate right
-        }
-    }
-
-    public void move()
-    {
-        double radians = Math.toRadians(direction);
-        int dx = (int)(SPEED * Math.cos(radians));
-        int dy = (int)(SPEED * Math.sin(radians));
-        setLocation(getX() + dx, getY() + dy);
     }
 }
--- Ship.java
+++ Ship.java
@@ -12,6 +12,7 @@ public class Ship extends Actor
      * Act - do whatever the Ship wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+<<<<<<< Updated upstream
     
     public Ship()
     {
@@ -21,5 +22,37 @@ public class Ship extends Actor
     public void act()
     {
         
+=======
+    static int SPEED = 10;
+    double direction = 0; // angle in degrees
+
+    public void act()
+    {
+        // Add your action code here.
+
+        if (Greenfoot.isKeyDown("space"))
+        {
+            move();
+        }
+
+        if (Greenfoot.isKeyDown("a"))
+        {
+            direction -= 1; // rotate left
+            System.out.println("Direction: " + direction);
+        }
+        if (Greenfoot.isKeyDown("d"))
+        {            
+            direction += 1; // rotate right
+            System.out.println("Direction: " + direction);
+        }
+    }
+
+    public void move()
+    {
+        double radians = Math.toRadians(direction);
+        int dx = (int)(SPEED * Math.cos(radians));
+        int dy = (int)(SPEED * Math.sin(radians));
+        setLocation(getX() + dx, getY() + dy);
+>>>>>>> Stashed changes
     }
 }
