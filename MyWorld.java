95318703386f32329eebce1bc2534f7769d8bb4c
348831199 <348831199@gapps.yrdsb.ca>
2026-05-08 12:03:11 -0400

    put ship at center of world & adjusted ship start angle

--- MyWorld.java
+++ MyWorld.java
@@ -4,6 +4,6 @@ public class MyWorld extends World {
     public MyWorld() {
         super(500, 600, 1);
         Ship ship = new Ship();
-        addObject(ship, 300, 200);
+        addObject(ship, 250, 300);
     }
 }
