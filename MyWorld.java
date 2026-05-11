3d882eb18782a6c596a8dbb1485c62f1230ace33
348831199 <348831199@gapps.yrdsb.ca>
2026-05-11 11:09:12 -0400

    borders removed

--- MyWorld.java
+++ MyWorld.java
@@ -2,7 +2,7 @@ import greenfoot.*;
 
 public class MyWorld extends World {
     public MyWorld() {
-        super(500, 600, 1);
+        super(500, 600, 1, false);
         Ship ship = new Ship();
         addObject(ship, 250, 300);
     }
