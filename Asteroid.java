a8d7f96758a22730a264a96d63a1e02d51121907
Jason <baifnjdn@gmail.com>
2026-05-11 11:43:34 -0400

    Worked on asteroid

--- Asteroid.java
+++ Asteroid.java
@@ -13,29 +13,32 @@ public class Asteroid extends Actor
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     
-    int speed;
+    int speed = 6;
     double direction;
     
     static int angleVariation = 30; // how much it varies from center 
     
-    public Asteroid(int speed, int x, int y)
+    public Asteroid(int x, int y)
     {
         GreenfootImage image = new GreenfootImage("button-green.png");
         setImage(image);
         
-        this.speed = speed;
-        
-        
-        
+        //double angle = Math.atan((double)(y - getWorld().getHeight() / 2.0) / (double)(x - getWorld().getWidth() / 2.0));
+        // if (x < getWorld().getWidth() / 2.0)
+        // {
+        //     // arctan's range is only -90 to 90, so we need to add 180 if it's coming from the left
+        //     angle += Math.PI; 
+        // }
+        // this.direction = angle * 180 / Math.PI + Greenfoot.getRandomNumber(angleVariation * 2) - angleVariation; // convert to degrees
     }
     public void act()
     {
         // Add your action code here.
-        move();
-        if (isAtEdge())
-        {
-            getWorld().removeObject(this);
-        }
+        // move();
+        // if (isAtEdge())
+        // {
+        //     getWorld().removeObject(this);
+        // }
     }
     public void move()
     {
--- MyWorld.java
+++ MyWorld.java
@@ -1,9 +1,35 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+
+    static int ASTEROID_SPAWN_RATE = 10;
+    static int invalidSpawnRadius = 150; // how close to the center we can spawn asteroids
+    static int outerSpawnRadius = 200; // how far from the center we can spawn asteroids
+    int time = 0;
+
     public MyWorld() {
         super(500, 600, 1, false);
         Ship ship = new Ship();
         addObject(ship, 250, 300);
+    }
+
+    public void act() {
+        time++;
+        if (time % ASTEROID_SPAWN_RATE == 0) {
+            spawnAsteroid();
+        }
+    }
+
+    private void spawnAsteroid() {
+        int x = Greenfoot.getRandomNumber(getWidth() + 2 * outerSpawnRadius) - outerSpawnRadius;
+        int y = Greenfoot.getRandomNumber(getHeight() + 2 * outerSpawnRadius) - outerSpawnRadius;
+        while ((x-getWidth()/2)*(x-getWidth()/2) + (y-getHeight()/2)*(y-getHeight()/2) < invalidSpawnRadius * invalidSpawnRadius)
+        {
+            // don't spawn too close to the center where the ship is
+            x = Greenfoot.getRandomNumber(getWidth() + 2 * outerSpawnRadius) - outerSpawnRadius;
+            y = Greenfoot.getRandomNumber(getHeight() + 2 * outerSpawnRadius) - outerSpawnRadius;
+        }
+        Asteroid asteroid = new Asteroid(x, y);
+        addObject(asteroid, x, y);
     }
 }
