a0e65e114161c6b3c6a786cf5b826bfe87fe93dc
Jason <baifnjdn@gmail.com>
2026-05-11 11:16:07 -0400

    Worked on asteroid

--- Asteroid.java
+++ Asteroid.java
@@ -12,13 +12,37 @@ public class Asteroid extends Actor
      * Act - do whatever the Asteroid wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public Asteroid()
+    
+    int speed;
+    double direction;
+    
+    static int angleVariation = 30; // how much it varies from center 
+    
+    public Asteroid(int speed, int x, int y)
     {
         GreenfootImage image = new GreenfootImage("button-green.png");
         setImage(image);
+        
+        this.speed = speed;
+        
+        
+        
     }
     public void act()
     {
         // Add your action code here.
+        move();
+        if (isAtEdge())
+        {
+            getWorld().removeObject(this);
+        }
+    }
+    public void move()
+    {
+        double radians = Math.toRadians(direction);
+        int dx = (int)(speed * Math.cos(radians));
+        int dy = (int)(speed * Math.sin(radians));
+        setLocation(getX() + dx, getY() + dy);
+
     }
 }
