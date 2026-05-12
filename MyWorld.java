843248e45c260cafc7b5186692bcfec67a78234b
Jason <baifnjdn@gmail.com>
2026-05-12 11:50:58 -0400

    Made world size consistent

--- MyWorld.java
+++ MyWorld.java
@@ -9,7 +9,7 @@ public class MyWorld extends World {
     int time = 0;
 
     public MyWorld() {
-        super(500, 600, 1, false);
+        super(WIDTH, HEIGHT, 1, false);
         super.setBackground("black_screen.jpeg");
         Ship ship = new Ship();
         addObject(ship, WIDTH / 2, HEIGHT / 2);
