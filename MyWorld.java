6737d48ae5f0bd7347f285a21b72161b2a403ef2
348831199 <348831199@gapps.yrdsb.ca>
2026-05-12 11:48:00 -0400

    fixed bugsk and background and asteroid error lol

--- MyWorld.java
+++ MyWorld.java
@@ -10,8 +10,10 @@ public class MyWorld extends World {
 
     public MyWorld() {
         super(500, 600, 1, false);
+        super.setBackground("black_screen.jpeg");
         Ship ship = new Ship();
         addObject(ship, WIDTH / 2, HEIGHT / 2);
+
     }
 
     public void act() {
