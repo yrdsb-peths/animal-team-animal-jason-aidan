95243d1f0ee368123635fef2072d5f885560500a
Jason <baifnjdn@gmail.com>
2026-05-14 11:54:03 -0400

    Fixed stats

--- MyWorld.java
+++ MyWorld.java
@@ -10,8 +10,8 @@ public class MyWorld extends World {
     static final int WIDTH = 600;
     static final int HEIGHT = 600;
 
-    static int ASTEROID_SPAWN_RATE = 500;
-    static int OUTER_SPAWN_RADIUS = 350; // how far from the center we can spawn asteroids
+    static int asteroidSpawnRate = 300;
+    static final int OUTER_SPAWN_RADIUS = 350; 
     int time = 0;
 
     public MyWorld() {
@@ -19,18 +19,18 @@ public class MyWorld extends World {
         super.setBackground("black_screen.jpeg");
         Ship ship = new Ship();
         addObject(ship, WIDTH / 2, HEIGHT / 2);
-        ASTEROID_SPAWN_RATE = 500;
+        asteroidSpawnRate = 300;
         time = 0;
     }
 
     public void act() {
         time++;
-        if (time % ASTEROID_SPAWN_RATE == 0 || time == 1) {
+        if (time % asteroidSpawnRate == 0 || time == 1) {
             spawnAsteroid();
         }
         if(time % 6000 == 0)
         {
-            ASTEROID_SPAWN_RATE -= 10;
+            asteroidSpawnRate -= 10;
         }
     }
 
@@ -41,7 +41,11 @@ public class MyWorld extends World {
         int y = HEIGHT/2  + (int)(OUTER_SPAWN_RADIUS * Math.sin(Math.toRadians(direction)));
         
 
-        Asteroid asteroid = new Asteroid(x, y, direction-180, LARGE);
+        Asteroid asteroid = new Asteroid(x, y, direction-180, Greenfoot.getRandomNumber(3));
         addObject(asteroid, x, y);
     }
+
+    public static void loseGame()
+    {
+        Greenfoot.stop();    }
 }
