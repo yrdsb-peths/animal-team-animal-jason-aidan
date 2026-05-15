ce7745fa6993c6c178a5a136e5d8223841d09561
Jason <baifnjdn@gmail.com>
2026-05-15 11:35:48 -0400

    Fixed hitboxes

--- Ship.java
+++ Ship.java
@@ -65,11 +65,6 @@ public class Ship extends Actor
         {
             fire();
         }
-
-        if (isTouching(Asteroid.class))
-        {
-            MyWorld.loseGame();
-        }
     }
 
     public void detectPosition()
